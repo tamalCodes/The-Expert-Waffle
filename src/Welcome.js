290ff9bd2e9aaca3cda574d6da3a9c9d8fe49176
Tamal Das <gyansujan69@gmail.com>
2022-06-19 09:38:25 +0530

    Updates on custom Welcome
    Simjoin - to stimulate the join
    Cache - to store the stuffs in cache prevent lag

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,6 +5,8 @@ const welcomeSchema = require("../models/WelcomeSchema");
 //^ !setwelcome <write the welcome message in here>
 
 module.exports = (client) => {
+  const cache = {}; // guildId: [channelId, text]
+
   command(client, "setwelcome", async (message) => {
     const { member, channel, guild, content } = message;
 
@@ -33,6 +35,8 @@ module.exports = (client) => {
     split.shift();
     text = split.join(" ");
 
+    cache[guild.id] = [channel.id, text];
+
     // finally putting it into the DB but also gotta make sure that we basically update the texts if it is already in the DB
 
     // upsert is basically the combo for insert and update !
@@ -56,5 +60,49 @@ module.exports = (client) => {
         mongoose.connection.close();
       }
     });
+  });
+
+  //^ Onjoin and Simjoin are there to stimulate the way the welcome message would appear
+
+  const onJoin = async (member) => {
+    const { guild } = member;
+
+    let data = cache[guild.id];
+
+    if (!data) {
+      console.log("FETCHING FROM DATABASE. . .");
+
+      await mongo().then(async (mongoose) => {
+        try {
+          const result = await welcomeSchema.findOne({ _id: guild.id });
+
+          cache[guild.id] = data = [result.channelId, result.text];
+        } finally {
+          mongoose.connection.close();
+        }
+      });
+    }
+
+    const channelId = data[0];
+    const text = data[1];
+
+    const channel = guild.channels.cache.get(channelId);
+    channel.send(text.replace(/<@>/g, `<@${member.id}>`));
+  };
+
+  command(client, "simjoin", (message) => {
+    if (!message.member.permissions.has("ADMINISTRATOR")) {
+      message.delete();
+
+      message.author.send(`<@${member.id}> You donot have such permissions !!`);
+
+      return;
+    }
+
+    onJoin(message.member);
+  });
+
+  client.on("guildMemberAdd", (member) => {
+    onJoin(member);
   });
 };
