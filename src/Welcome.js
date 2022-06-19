17bb37a12724467d5f9803f824e19201f33289a9
Tamal Das <gyansujan69@gmail.com>
2022-06-19 09:15:13 +0530

    Connected DB with !setwelcome

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,36 +1,60 @@
-const { MessageEmbed } = require("discord.js");
+const mongo = require("../database/Db");
+const command = require("./Command");
+const welcomeSchema = require("../models/WelcomeSchema");
+
+//^ !setwelcome <write the welcome message in here>
 
 module.exports = (client) => {
-  client.on("guildMemberAdd", (member) => {
-    const { user, guild } = member;
+  command(client, "setwelcome", async (message) => {
+    const { member, channel, guild, content } = message;
 
-    if (!user) {
+    if (!member.permissions.has("ADMINISTRATOR")) {
+      message.delete();
+
+      message.author.send(`<@${member.id}> You donot have such permissions !!`);
+
       return;
     }
 
-    const channel = member.guild.channels.cache.get(
-      process.env.BOT_WELCOME_CHANNEL_ID
-    );
+    // i want the message to be checked so that the user has inputted stuffs after !setwelcome
 
-    const exampleEmbed = new MessageEmbed()
-      .setColor("#0099ff")
-      .setTitle("A new member has joined our guild !!")
-      .setAuthor({
-        name: user.tag,
-        iconURL: user.displayAvatarURL(),
-      })
-      .setDescription(
-        `Hello ${
-          user.username
-        } , welcome to Hustlers. I am the Expert waffle. Start with some ${guild.channels.cache
-          .get(process.env.BOT_START_WITH_ID)
-          .toString()} , and have fun.`
-      )
-      .setFooter({
-        text: `ID: ${user.id}`,
-      })
-      .setTimestamp();
+    // so i will make the content an array and will also delete "setwelcome" from the message so that it doesnot ends up in the Database
 
-    channel.send({ embeds: [exampleEmbed] });
+    // so that < !setwelcome hello buddy >  will be ["hello" , "buddy"]
+
+    let text = content;
+    const split = text.split(" ");
+
+    if (split.length < 2) {
+      message.author.send("Please provide a welcome message");
+      return;
+    }
+
+    split.shift();
+    text = split.join(" ");
+
+    // finally putting it into the DB but also gotta make sure that we basically update the texts if it is already in the DB
+
+    // upsert is basically the combo for insert and update !
+
+    await mongo().then(async (mongoose) => {
+      try {
+        await welcomeSchema.findOneAndUpdate(
+          {
+            _id: guild.id,
+          },
+          {
+            _id: guild.id,
+            channelId: channel.id,
+            text,
+          },
+          {
+            upsert: true,
+          }
+        );
+      } finally {
+        mongoose.connection.close();
+      }
+    });
   });
 };
